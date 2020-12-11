d7fdba0218700095069e86e3c8e08639a57c8513
Marcelino Meza <kmeza@ucol.mx>
2020-12-11 11:04:28 -0600

    addes sumarPares function

--- src/main.js
+++ src/main.js
@@ -1,3 +1,59 @@
-export default class App {}
+export default class App {
+
+    // Función que determine la suma de los pares entre 1 y 20 (for)
+
+    sumarParesFor(){
+
+        let suma = 0;
+
+        for(let i=1; i<=20; i=i+1){
+            if(i%2===0){
+                suma = suma + i;
+            }
+        }
+
+        return suma;
+
+    }
+
+    // Función que determine cuantos números impares hay en un intervalo (for)
+
+    A
+    
+    // Función que  (while)
+
+    A
+
+    // Función que  (while)
+
+    A
+
+    // Función que (do while)
+
+    A
+
+    // Función que (do while)
+
+    A
+
+}
 
 let app = new App();
+
+// F1
+console.log(app.sumarParesFor());
+
+// F2
+
+
+// F3
+
+
+// F4
+
+
+// F5
+
+
+// F6
+
